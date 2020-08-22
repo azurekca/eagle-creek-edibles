9b0ace6e913a35117804b92646636e0bcd5154ea
Agata Zurek <agata.w.zurek@gmail.com>
2020-08-21 21:25:31 -0700

    temp fix: not showing image for now

--- src/admin/preview-templates/recipe.js
+++ src/admin/preview-templates/recipe.js
@@ -7,17 +7,19 @@ const Recipe = createClass({
 	render() {
     const entry = this.props.entry;
     const title = entry.getIn([ 'data', 'title' ], null)
-    const rating = entry.getIn([ 'data', 'rating'], null);
     const image = entry.getIn(['data', 'image'], null);
-    const previewImg = this.props.getAsset(image);
+    const previewImg = this.props.getAsset(image).toString();
+    const rating = entry.getIn([ 'data', 'rating'], null);
+    let stars = '';
+    for (let i = 0; i < rating; i++) {
+      stars += '★';
+    }
 
     return html`
       <div class="text-wrapper flow">
         <h1>${title}</h1>
         <div class="flex">
-          <p class="inline stars">
-            ${rating} stars
-          </p>
+          <p class="inline stars">${stars}</p>
           <p class="inline">🔪 Prep time: ${entry.getIn([ 'data', 'prepTime' ], null)}</p>
           <p class="inline">⏱️ Cooking time: ${entry.getIn([ 'data', 'cookTime' ], null)}</p>
         </div>
@@ -32,7 +34,6 @@ const Recipe = createClass({
         </ul>
         <h2>Instructions</h2>
         ${this.props.widgetFor('body')}
-        <img src="${previewImg.toString()}" alt="${title}">
       </div
     `;
 	}
