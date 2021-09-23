1c25326d67c5c465ba6a69ba1dc47359f1ce5a51
Agata Zurek <agata.w.zurek@gmail.com>
2021-09-23 08:41:18 -0700

    style: prettier

--- src/admin/preview-templates/recipe.js
+++ src/admin/preview-templates/recipe.js
@@ -4,12 +4,12 @@ const html = htm.bind(h);
 
 // Preview component for a recipe
 const Recipe = createClass({
-	render() {
+  render() {
     const entry = this.props.entry;
-    const title = entry.getIn([ 'data', 'title' ], null)
+    const title = entry.getIn(['data', 'title'], null);
     const image = entry.getIn(['data', 'image'], null);
     const previewImg = this.props.getAsset(image).toString();
-    const rating = entry.getIn([ 'data', 'rating'], null);
+    const rating = entry.getIn(['data', 'rating'], null);
     let stars = '';
     for (let i = 0; i < rating; i++) {
       stars += '★';
@@ -20,24 +20,25 @@ const Recipe = createClass({
         <h1>${title}</h1>
         <div class="flex">
           <p class="inline stars">${stars}</p>
-          <p class="inline">🔪 Prep time: ${entry.getIn([ 'data', 'prepTime' ], null)}</p>
-          <p class="inline">⏱️ Cooking time: ${entry.getIn([ 'data', 'cookTime' ], null)}</p>
+          <p class="inline">
+            🔪 Prep time: ${entry.getIn(['data', 'prepTime'], null)}
+          </p>
+          <p class="inline">
+            ⏱️ Cooking time: ${entry.getIn(['data', 'cookTime'], null)}
+          </p>
         </div>
         <h2>Ingredients</h2>
         <ul>
-          ${entry.getIn([ 'data', 'ingredients' ], []).map(
-            ingredient =>
-              html`
-                  <li>${ingredient}</li>
-                `
-          )}
+          ${entry
+            .getIn(['data', 'ingredients'], [])
+            .map(ingredient => html` <li>${ingredient}</li> `)}
         </ul>
         <h2>Instructions</h2>
         ${this.props.widgetFor('body')}
-        <img src="${previewImg}" alt="${title}"/>
+        <img src="${previewImg}" alt="${title}" />
       </div>
     `;
-	}
+  },
 });
 
 export default Recipe;
